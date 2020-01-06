5e1c0f94245c1ad3b1dc5fac190d41990960b1c5
pfub1337 <pfuber@ya.ru>
2020-01-06 22:29:54 +0300

    добавлен второй уровень изменены текстуры персонажа добавлен класс для тригеров добавлены временные текстуры для триггеров добавлена функция миссии и обозначение для мапмейкинга "/" добавлена активация миссии на букву E (англ) 01.01 Переделаны миссии, добавлен конец миссии (синий квадрат) Вывод текста миссии при попадании игрока на триггер миссии (красный квадрат) Добавлены возможности отклонить миссию Добавлены награды за миссии Добавлена прокачка персонажа 05.01 Добавлено сохренение прогресса в файл save.txt 1-ая строчка сохранения - опыт персонажа 2-ая строчка сохранения - уровень прокачки скорости 3-я строчка сохранения - скорость персонажа 06.01 Добавлена переменная здоровья Добавлена лава, наносящая урон Изменена карта

--- game_proj.py
+++ game_proj.py
@@ -1,19 +1,24 @@
 import pygame
 import os
 import sys
+import time
 
-
-FPS = 60
+FPS = 30
 pygame.init()
 size = WIDTH, HEIGHT = 550, 550
 MOVE_SPEED = 10
 screen = pygame.display.set_mode(size)
 clock = pygame.time.Clock()
-# filename = input()
 MISSON_ACTIVE = False
+HP = 5
 EXP = 0
 SPEED_UP = 1
 pygame.key.set_repeat(1, 50)
+
+# def wait(secs):
+#     while time.sleep(secs):
+#         return False
+#     return True
 
 def load_image(name, colorkey=None):
     fullname = os.path.join('data', name)
@@ -71,7 +76,7 @@ def start_screen():
             if event.type == pygame.QUIT:
                 terminate()
             elif event.type == pygame.KEYDOWN or event.type == pygame.MOUSEBUTTONDOWN:
-                return  # начинаем игру
+                return
         pygame.display.flip()
         clock.tick(FPS)
 
@@ -104,7 +109,7 @@ def Mission():
                         MISSON_ACTIVE = True
                         return
                     elif event.key == pygame.K_TAB:
-                        return
+                        return False
             pygame.display.flip()
             clock.tick(FPS)
 
@@ -177,43 +182,70 @@ def experience():
     exp_render = font.render('Exp: ' + str(EXP), 1, (100, 255, 0))
     screen.blit(exp_render, (20, 20))
 
+def health():
+    global HP
+    font = pygame.font.Font(None, 40)
+    hp_render = font.render('HP: ' + str(HP), 1, (100, 255, 0))
+    screen.blit(hp_render, (20, 50))
+
+def DeadScreen():
+    font = pygame.font.Font(None, 40)
+    text_render = font.render('YOU DIE', 1, (200, 0, 0))
+    text_x = WIDTH // 2 - text_render.get_width() // 2
+    text_y = HEIGHT // 2 - text_render.get_height() // 2
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                terminate()
+            # elif event.type == pygame.KEYDOWN:
+            #     if event.key == pygame.K_ESCAPE:
+            #         # start_screen()
+            #         loadGame()
+            #         # player, level_x, level_y = generate_level(load_level('testlevel.txt'))
+            #         return
+        screen.blit(text_render, (text_x, text_y))
+        pygame.display.flip()
+
 def upgrades():
     global EXP
     global SPEED_UP
     global MOVE_SPEED
+    upgrades = ['1. SPEED UPGRADE - ' + str(int(100 * (SPEED_UP * 0.5))) + 'EXP', '2. HEALTH UPGRADE -']
     font = pygame.font.Font(None, 40)
-    upgrades_render = font.render('1. SPEED UPGRADE' + str(100 * (SPEED_UP * 0.5)), 1, (255, 255, 255))
+    pygame.draw.rect(screen, (50, 50, 75), (50, 241, 450, 68), 0)
+    upgrades_render = font.render('1. SPEED UPGRADE - ' + str(int(100 * (SPEED_UP * 0.5))) + 'EXP', 1, (255, 255, 255))
+    cost_render = font.render(str(100 * (SPEED_UP * 0.5)), 1, (255, 255, 255))
     text_x = WIDTH // 2 - upgrades_render.get_width() // 2
     text_y = HEIGHT // 2 - upgrades_render.get_height() // 2
     text_w = upgrades_render.get_width()
     text_h = upgrades_render.get_height()
+    print(text_x, text_y, text_h, text_w)
     screen.blit(upgrades_render, (text_x, text_y))
-    pygame.draw.rect(screen, (255, 255, 255), (text_x - 10, text_y - 10, text_w + 20, text_h + 20), 1)
+    # pygame.draw.rect(screen, (255, 255, 255), (text_x - 10, text_y - 10, text_w + 20, text_h + 20), 1)
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 terminate()
             elif event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_1 and EXP >= 100 * (SPEED_UP * 0.5):
+                if event.key == pygame.K_1 and EXP >= 100 * (SPEED_UP * 0.5) and SPEED_UP <= 10:
                     print('Upgrade bought')
                     SPEED_UP += 1
-                    MOVE_SPEED += 5
+                    MOVE_SPEED += 2
                     EXP -= 100 * (SPEED_UP * 0.5)
+                    print(100 * (SPEED_UP * 0.5))
+                    pygame.time.wait(500)
+                    return True
                 elif event.key == pygame.K_ESCAPE:
                     return
         pygame.display.flip()
         clock.tick(FPS)
 
-
-
-
-# основной персонаж
 player = None
 
-# группы спрайтов
 all_sprites = pygame.sprite.Group()
 walls_group = pygame.sprite.Group()
 floor_group = pygame.sprite.Group()
+damage_group = pygame.sprite.Group()
 startMisTrig_group = pygame.sprite.Group()
 endMisTrig_group = pygame.sprite.Group()
 player_group = pygame.sprite.Group()
@@ -233,9 +265,11 @@ def generate_level(level):
                 StartMissionTrigger('start_mission_trigger', x, y)
             elif level[y][x] == '-':
                 EndMissionTrigger('end_mission_trigger', x, y)
+            elif level[y][x] == 'x':
+                Floor('lava', x, y)
     return new_player, x, y
 
-floor_images = {'floor': load_image('floor.png')}
+floor_images = {'floor': load_image('floor.png'), 'lava' : load_image('lava.png')}
 startMisTrig_images = {'start_mission_trigger': load_image('trigger.png')}
 endMisTrig_images = {'end_mission_trigger': load_image('trigger2.png')}
 walls_images = {'wall': load_image('box.png')}
@@ -254,9 +288,14 @@ class Walls(pygame.sprite.Sprite):
 
 class Floor(pygame.sprite.Sprite):
     def __init__(self, tile_type, pos_x, pos_y):
-        super().__init__(floor_group, all_sprites)
-        self.image = floor_images[tile_type]
-        self.rect = self.image.get_rect().move(tile_width * pos_x, tile_height * pos_y)
+        if tile_type == 'floor':
+            super().__init__(floor_group, all_sprites)
+            self.image = floor_images[tile_type]
+            self.rect = self.image.get_rect().move(tile_width * pos_x, tile_height * pos_y)
+        elif tile_type == 'lava':
+            super().__init__(damage_group, all_sprites)
+            self.image = floor_images[tile_type]
+            self.rect = self.image.get_rect().move(tile_width * pos_x, tile_height * pos_y)
 
 class StartMissionTrigger(pygame.sprite.Sprite):
     def __init__(self, tile_type, pos_x, pos_y):
@@ -278,7 +317,8 @@ class Player(pygame.sprite.Sprite):
         self.rect = self.image.get_rect().move(tile_width * pos_x + 15, tile_height * pos_y + 5)
 
     def update(self, keys):
-        global MISSON_ACTIVE
+        # global MISSON_ACTIVE
+        global HP
         if keys[pygame.K_LEFT] == 1:
             self.rect.x -= MOVE_SPEED
             self.image = player_left
@@ -300,26 +340,33 @@ class Player(pygame.sprite.Sprite):
             if pygame.sprite.spritecollideany(self, walls_group):
                 self.rect.y += MOVE_SPEED
         if pygame.sprite.spritecollideany(self, startMisTrig_group) and not MISSON_ACTIVE:
+            if not Mission():
+                pygame.time.set_timer(pygame.KEYDOWN, 1000)
             Mission()
         if pygame.sprite.spritecollideany(self, endMisTrig_group) and MISSON_ACTIVE:
             endMission()
         if keys[pygame.K_ESCAPE] == 1:
             Pause()
         if keys[pygame.K_e] == 1:
-            upgrades()
+            while upgrades():
+                upgrades()
+        if pygame.sprite.spritecollideany(self, damage_group):
+            HP -= 1
+            # pygame.time.set_timer(pygame.sprite.spritecollideany(self, damage_group), 1000)
+            if HP <= 0:
+                DeadScreen()
+
+
 
 class Camera:
-    # зададим начальный сдвиг камеры
     def __init__(self):
         self.dx = 0
         self.dy = 0
 
-    # сдвинуть объект obj на смещение камеры
     def apply(self, obj):
         obj.rect.x += self.dx
         obj.rect.y += self.dy
 
-    # позиционировать камеру на объекте target
     def update(self, target):
         self.dx = -(target.rect.x + target.rect.w // 2 - WIDTH // 2)
         self.dy = -(target.rect.y + target.rect.h // 2 - HEIGHT // 2)
@@ -336,13 +383,12 @@ while running:
             saveGame()
         if event.type == pygame.KEYDOWN:
             player_group.update(pygame.key.get_pressed())
-    # изменяем ракурс камеры
     camera.update(player)
-    # обновляем положение всех спрайтов
     for sprite in all_sprites:
         camera.apply(sprite)
     screen.fill((25, 25, 25))
     all_sprites.draw(screen)
     player_group.draw(screen)
     experience()
+    health()
     pygame.display.flip()
